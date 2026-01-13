bb3664e1ab8b6cc6a4b35f08f026f1ba65e8db4d
nmquys <mquy2810@gmail.com>
2026-01-13 22:30:39 +0700

    update: get lines from channel

--- main.go
+++ main.go
@@ -4,7 +4,42 @@ import(
 	"fmt"
 	"os"
 	"log"
+	"bytes"
+	"io"
 )
+
+func getLinesChannel(f io.ReadCloser) <- chan string {
+	out := make(chan string, 1)
+
+	go func(){
+		defer f.Close()
+		defer close(out)
+
+		currentLine := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				currentLine += string(data[:i])
+				data = data[i + 1:]
+				out <- currentLine
+				currentLine = ""
+			}
+			currentLine += string(data)
+		}
+
+		if len(currentLine) != 0 {
+			out <- currentLine
+		}
+	}()
+
+	return out
+}
 
 func main(){
 	f, err := os.Open("message.txt")
@@ -12,14 +47,8 @@ func main(){
 		log.Fatal("error", "error", err)
 	}
 
-	for {
-		data := make([]byte, 8)
-		readByte, err := f.Read(data)
-
-		if err != nil {
-			break
-		}
-
-		fmt.Printf("read: %s\n", string(data[:readByte]))
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
